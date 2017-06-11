bc059c4ba1e6417396e0587e96526b276e3f0bdc
Doug Hellmann <doug@doughellmann.com>
2017-06-11 17:10:24 -0400

    commit template

---
+++ bin/rsync.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+set -x
+
+FILES="*.html css js lib plugin *.svg img fonts"
+
+ssh doughellmann.com 'mkdir -p ~/doughellmann.com/presentations/python-context-managers'
+
+rsync -av --progress $FILES doughellmann.com:~/doughellmann.com/presentations/python-context-managers/
