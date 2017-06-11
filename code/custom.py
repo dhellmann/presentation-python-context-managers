b44adeafa90d5f02a55020613b6a44fbaffe6772
Doug Hellmann <doug@doughellmann.com>
2017-06-11 17:34:10 -0400

    show how to create classes that follow the contextmanager API

--- code/custom.py
+++ code/custom.py
@@ -1,22 +1,14 @@
-#!/usr/bin/env python
-
-"""Defining your own context manager.
-"""
-
 class MyContext(object):
     def __init__(self):
-        print '__init__()'
-    
+        print('__init__()')
+
     def __enter__(self):
-        print '__enter__() (acquire the resource)'
+        print('__enter__() (acquire the resource)')
         return 5
-    
+
     def __exit__(self, exc_type, exc_val, exc_tb):
-        print '__exit__() (release the resource)'
-        print str(exc_val)
+        print('__exit__() (release the resource)')
         return True
 
-print
 with MyContext() as c:
-    print 'Doing work in the context', c
-    raise RuntimeError('hi!')
+    print('Doing work in the context', c)
